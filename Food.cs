67840ea6217dcd53886730b77442ce46c537f427
Nilesh Dhawade <dhawade.nilesh007@gmail.com>
2022-03-17 08:33:24 +0530

    new commit

---
+++ Food.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tsystem_Day4_Assignments
+{
+    public class Food
+    {
+        string fName;
+        double fprice;
+
+        public Food()
+        {
+            fName = "";
+            fprice = 0.0;
+        }
+        public Food(string fname,double fprice)
+        {
+            this.fName = fname;
+            this.fprice = fprice;
+        }
+        public override string ToString()
+        {
+            return "name of the food is " + fName + " food price is " + fprice; 
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,8 +10,21 @@ namespace Tsystem_Day4_Assignments
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
-            Console.WriteLine("hello");
+            List<Food> f1 = new List<Food>();
+
+            Food fd = new Food("vadapav", 15.0);
+            Food fd1 = new Food("samosa", 20.0);
+            Food fd2 = new Food("dosa", 40.0);
+
+            f1.Add(fd);
+            f1.Add(fd1);
+            f1.Add(fd2);
+
+            foreach (Food item in f1)
+            {
+                Console.WriteLine(item);
+
+            }
         }
     }
 }
